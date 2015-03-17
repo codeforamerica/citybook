4fdb91cf7b7398a0c0f0c378a9c5092f2b31e2df
svmatthews <matthews.sam@gmail.com>
2015-03-16 17:15:47 -0700

    social and health services all wrapped up in one, searchable place!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rva-searchable-services
-Connects Google Spreadsheets to Search
+# Richmond, VA - Health Services List
+
+Despite the many existing health and social services in the city of Richmond, organizations have trouble keeping their lists up-to-date. Many people making referrals have their own up-to-date list in a Google spreadsheet or other formats but they are typically not shared with other people and their data are not available to the public.
+
+This project takes a multi-sheet Google spreadsheet of health and social services in Richmond, VA and turns them into a searchable list. The data are provided by the [Center on Society and Health](http://www.societyhealth.vcu.edu/) at VCU. This is a project by the [2015 Richmond Fellowship team](http://rva.codeforamerica.org).
+
+### Status
+
+This is an ongoing project and is not intended for official use.
+
+### What is this built with?
+
+This project uses [Tabletop.js](https://github.com/jsoma/tabletop) for the spreadsheet access, [Handlebars](http://handlebarsjs.com/) for some templating, [List.js](http://www.listjs.com/) for the searchable list, and [Bootstrap](http://getbootstrap.com/) for the styles.
---
+++ static/site.js
@@ -0,0 +1,160 @@
+var list, source, template, serviceType;
+
+function init() {
+
+  // init hanlebars
+  var serviceTemplate = '<li class="col-md-12 service {{serviceClass}}">\
+                          <h2 class="col-md-12 title">{{title}}</h2>\
+                          <div class="col-md-10 description"><strong>Description: </strong>{{description}}</div>\
+                          <div class="col-md-10 population"><strong>Population: </strong>{{population}}</div>\
+                          <div class="col-md-10 criteria"><strong>Criteria: </strong>{{criteria}}</div>\
+                          <div class="col-md-4">\
+                          <div class="col-md-12"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span><strong>Contact</strong></div>\
+                            <div class="col-md-12 contact">{{contact}}</div>\
+                          </div>\
+                          <div class="col-md-4">\
+                            <div class="col-md-12"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span><strong>Cost</strong></div>\
+                            <div class="col-md-12 cost">{{cost}}</div>\
+                          </div>\
+                          <div class="col-md-4">\
+                          </div>\
+                        </li>';
+  template = Handlebars.compile(serviceTemplate);
+  
+  // get the list information via tabletop
+  getList.tabletop();
+
+  // use this if using get.py
+  // getList.local();
+}
+
+var getList = {
+  tabletop: function() {
+    console.log('FUNCTION: getList.tabletop()');
+    Tabletop.init({
+      key: '1liPb1u_Z09Du8L--OjNWl_zZi7KE0_-ejIHw5OfkslQ',
+      callback: success,
+    })
+  },
+  local: function() {
+    $.ajax({
+      url: '../list.json',
+      dataType: 'json',
+      success: success,
+      error: function(err) {
+        console.error(err);
+      }
+    });
+  }
+}
+
+/* 
+callback function after the list data has
+been returned successfully. Add it to `list`
+so we have access to the information globally */
+function success(data) {
+  list = data;
+  listLoop();
+}
+
+function initSearch() {
+  console.log('FUNCTION: initSearch()');
+  var options = {
+    valueNames: [ 'title', 'description', 'population', 'criteria', 'contact', 'cost' ]
+  }
+  var searchList = new List('service-list-wrapper', options);
+  console.log(searchList);
+}
+
+function listLoop() {
+  firstFilter();
+  for (var key in list) {
+
+    // let's run this IIFE function to keep our 
+    // for loop scope while we go through it
+    // 'sheet' is the spreadsheet's tabulated sheet and represents list[key]
+    (function(sheet) {
+
+      // name of the service, originally from the sheet name
+      serviceType = key.toString();
+
+      // create a filter button for each serviceType
+      createFilter(serviceType, sanitize(serviceType));
+
+      // now lets get each row as "service" in k
+      sheet.elements.forEach(handleService);
+      
+
+    })(list[key]);
+
+  }
+
+  // initialize the searchable list now that it has content
+  initSearch();
+}
+
+
+function handleService(service, index, array) {
+  
+  // set data for usage in handlebar template
+  var data = {
+    title: service['Provider/Program'],
+    description: service['Description'],
+    serviceClass: sanitize(serviceType),
+    cost: service['Cost/Fee'],
+    contact: service['Contact Info'],
+    population: service['Population Served'],
+    criteria: service['Criteria']
+  }
+
+  // append handlebar response to service list
+  document.getElementById('services-list').innerHTML += template(data);
+
+}
+
+function firstFilter() {
+  var servicesFilterList = document.getElementById('services-filter');
+  var all = document.createElement('button');
+  all.innerHTML = 'All';
+  all.className = 'service-filter btn btn-success active';
+  all.setAttribute('data', 'all');
+  all.setAttribute('type', 'button');
+  all.addEventListener('click', filterClick, false);
+  servicesFilterList.appendChild(all);
+}
+
+function createFilter(type, sanitized) {
+  var servicesFilterList = document.getElementById('services-filter');
+  var filter = document.createElement('button');
+  filter.innerHTML = type;
+  filter.className = 'service-filter btn btn-success';
+  filter.setAttribute('data', sanitized);
+  filter.setAttribute('type', 'button');
+  filter.addEventListener('click', filterClick, false);
+  servicesFilterList.appendChild(filter);
+}
+
+function sanitize(string) {
+  var s = string.replace(/[^A-Z0-9]/ig, "-");
+  s = s.toLowerCase();
+  return s;
+}
+
+function filterClick() {
+
+  // using jQuery here for simplicity
+  if(!$(this).hasClass('active')) {
+    var show = $(this).attr('data');
+    $('.service-filter').removeClass('active');
+    $(this).addClass('active');
+    if(show=='all') {
+      $('.service').show();  
+    } else {
+      $('.service').hide();
+      $('.'+show).show();
+    }
+  }  
+
+}
+
+window.onload = init();
