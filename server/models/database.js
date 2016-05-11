867bd5dcb17d661a898e1246b932c1bf0eb363d1
Charles Weems <charlie.weems@gmail.com>
2016-05-10 19:32:11 -0700

    Add ToS modal. Closes #46

--- server/models/database.js
+++ server/models/database.js
@@ -5,7 +5,8 @@ var client = new pg.Client(connectionString);
 
 client.connect();
 
-var query = client.query('CREATE TABLE book_links(id SERIAL PRIMARY KEY, title VARCHAR(160) not null, link VARCHAR(160) not null, opt_in BOOLEAN)');
+var query = client.query(
+  'CREATE TABLE citybooks_list(id SERIAL PRIMARY KEY, title VARCHAR(160) not null, link VARCHAR(160) not null, email VARCHAR(160) not null, location VARCHAR(160), opt_in BOOLEAN)');
 
 query.on('end', function() {
   client.end();
