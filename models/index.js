f49882cb9d80b53d0a0ab5bb931dc307b2c761e8
Charles Weems <charlie.weems@gmail.com>
2016-06-02 01:01:41 -0700

    Try to connect database

--- models/index.js
+++ models/index.js
@@ -12,8 +12,7 @@ if (process.env.DATABASE_URL) {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
+      host:     process.env.DATABASE_URL,
       logging:  true //false
     });
 } else {
