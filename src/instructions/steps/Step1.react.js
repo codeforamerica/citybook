5e16c9fe68ff3a25a9826e24f3f2bee78a7c6074
Charles Weems <charlie.weems@gmail.com>
2016-06-14 12:08:31 -0700

    React link validation

--- src/instructions/steps/Step1.react.js
+++ src/instructions/steps/Step1.react.js
@@ -10,7 +10,7 @@ export default class Step1 extends Component {
           <p>Build a new Google Spreadsheet using the CityBook template. You'll need to copy the template to your own Google Drive account by clicking the "Use this template" button.</p>
         </Col>
         <Col md={4}>
-          <Button className="btn-blue" bsSize="large" href="https://drive.google.com/previewtemplate?id=18YIZlfxxlffHVytbR8zY1cvmKShGOK3z_Cf9JYow4pg&mode=public" >Get the Template</Button>
+          <Button className="btn-blue" bsSize="large" href="https://drive.google.com/previewtemplate?id=18YIZlfxxlffHVytbR8zY1cvmKShGOK3z_Cf9JYow4pg&mode=public" target='_blank'>Get the Template</Button>
         </Col>
       </Row>
     )
