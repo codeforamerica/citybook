0fb9e8cd79472ad4197bcc2f23cbd06a90758cc9
Keith Kurson <keith@keithkurson.net>
2016-06-27 14:26:16 -0700

    Google analytics

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import { Router, Route, Link, hashHistory } from 'react-router'
 import Home from './Home.react.js';
 import App from './App';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-79927051-1');
+
+
 import '../styles/styles.scss';
 
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={hashHistory} onUpdate={logPageView}>
     <Route path="/" component={Home} />
     <Route path="/books" component={App} />
     <Route path="/books/:bookId" component={App} />
