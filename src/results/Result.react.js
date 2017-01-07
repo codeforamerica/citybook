5927f1f86e8cf5dabc03af048a21ab1b0bbbd55d
Charles Weems <charlie.weems@gmail.com>
2017-01-07 15:17:49 -0800

    Better mobile friendly layout

--- src/results/Result.react.js
+++ src/results/Result.react.js
@@ -34,18 +34,26 @@ export default class Result extends Component {
             <p>{ telephone }</p>
           </Col>
           <Col xs={12} sm={7} className='button-container'>
-              <Button bsSize='large' className='result-button green' href={'tel:'+telephone}>
+            <Row>
+            <Col xs={4}>
+              <Button bsSize='large' className='result-button btn-block' href={'tel:'+telephone}>
                 <Glyphicon className='result-button-icon' glyph='earphone' />
                 <span className="result-button-label">Call Phone</span>
               </Button>
-              <Button bsSize='large' className='result-button' target='_blank' href={'https://maps.google.com/?q=' + address}>
+            </Col>
+            <Col xs={4}>
+              <Button bsSize='large' className='result-button btn-block' target='_blank' href={'https://maps.google.com/?q=' + address}>
                 <Glyphicon className='result-button-icon' glyph='map-marker' />
                 <span className="result-button-label">Open Map</span>
               </Button>
-              <Button bsSize='large' className='result-button' href="#" onClick={ ()=> this.setState({ panelOpen: !this.state.panelOpen })}>
+            </Col>
+            <Col xs={4}>
+              <Button bsSize='large' className='result-button btn-block' href="#" onClick={ ()=> this.setState({ panelOpen: !this.state.panelOpen })}>
                 <Glyphicon className='result-button-icon' glyph={ panelOpenIcon + '-sign'} />
                 <span className="result-button-label">Details</span>
               </Button>
+            </Col>
+            </Row>
           </Col>
         </Row>
         <Panel className='more-info' collapsible expanded={this.state.panelOpen}>
